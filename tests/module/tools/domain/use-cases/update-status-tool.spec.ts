a655316bf874869070c2ae8deba2358057cd03ce
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-13 13:19:17 -0300

    test: ensure UpdateStatusToolUseCase throw NotFoundError if LoadToolByIdRepository return undefined

--- tests/module/tools/domain/use-cases/update-status-tool.spec.ts
+++ tests/module/tools/domain/use-cases/update-status-tool.spec.ts
@@ -1,13 +1,19 @@
+import { NotFoundError } from '@/common/errors'
 import { type UpdateStatusTool, UpdateStatusToolUseCase } from '@/module/tools/domain/use-cases'
 import { toolsParams } from '@/tests/mocks'
-import { mock } from 'jest-mock-extended'
 import { type LoadToolByIdRepository } from '@/module/tools/domain/contracts/database/tools'
+
+import { mock } from 'jest-mock-extended'
 
 describe('UpdateStatusToolUseCase', () => {
   let sut: UpdateStatusTool
   const toolsRepository = mock<LoadToolByIdRepository>()
-  const { id } = toolsParams
+  const { id, name, dateOfCollection, dateOfDevolution, description, mechanicName, status } = toolsParams
   const StringId = String(id)
+
+  beforeAll(() => {
+    toolsRepository.loadById.mockResolvedValue({ id, name, dateOfCollection, dateOfDevolution, description, mechanicName, status })
+  })
 
   beforeEach(() => {
     sut = new UpdateStatusToolUseCase(toolsRepository)
@@ -18,5 +24,13 @@ describe('UpdateStatusToolUseCase', () => {
 
     expect(toolsRepository.loadById).toHaveBeenCalledWith({ id })
     expect(toolsRepository.loadById).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw NotFoundError if LoadToolByIdRepository return undefined', async () => {
+    toolsRepository.loadById.mockResolvedValueOnce(undefined)
+
+    const promise = sut.update({ id: StringId })
+
+    await expect(promise).rejects.toThrow(new NotFoundError('tool'))
   })
 })
