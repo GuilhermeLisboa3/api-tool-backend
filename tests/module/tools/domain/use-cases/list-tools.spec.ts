cb45646c5ba6e9dde61263e55c49a4bb2bcc1db2
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-13 01:18:58 -0300

    test: ensure ListToolsUseCase return list tools on success

--- tests/module/tools/domain/use-cases/list-tools.spec.ts
+++ tests/module/tools/domain/use-cases/list-tools.spec.ts
@@ -1,10 +1,16 @@
 import { ListToolsUseCase, type ListTools } from '@/module/tools/domain/use-cases'
 import { mock } from 'jest-mock-extended'
 import { type ListToolsRepository } from '@/module/tools/domain/contracts/database/tools'
+import { toolsParams } from '@/tests/mocks'
 
 describe('ListToolsUseCase', () => {
   let sut: ListTools
+  const { id, name, dateOfCollection, dateOfDevolution, description, mechanicName, status } = toolsParams
   const toolsRepository = mock<ListToolsRepository>()
+
+  beforeAll(() => {
+    toolsRepository.list.mockResolvedValue([{ id, name, dateOfCollection, dateOfDevolution, description, mechanicName, status }])
+  })
 
   beforeEach(() => {
     sut = new ListToolsUseCase(toolsRepository)
@@ -15,5 +21,11 @@ describe('ListToolsUseCase', () => {
 
     expect(toolsRepository.list).toHaveBeenCalledWith()
     expect(toolsRepository.list).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return list tools on success', async () => {
+    const result = await sut.list()
+
+    expect(result).toEqual([{ id, name, dateOfCollection, dateOfDevolution, description, mechanicName, status }])
   })
 })
