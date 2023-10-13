35a820f9af3cfcf0378a02650cf045bad9678aaa
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-13 18:45:28 -0300

    test: ensure ReserveToolUseCase throw NotFoundError if LoadToolByIdRepository return undefined

--- tests/module/tools/domain/use-cases/reserve.tool.spec.ts
+++ tests/module/tools/domain/use-cases/reserve.tool.spec.ts
@@ -2,14 +2,19 @@ import { type ReserveTool, ReserveToolUseCase } from '@/module/tools/domain/use-
 import { toolsParams } from '@/tests/mocks'
 import { mock } from 'jest-mock-extended'
 import { type LoadToolByIdRepository } from '@/module/tools/domain/contracts/database/tools'
+import { NotFoundError } from '@/common/errors'
 
 describe('ReserveToolUseCase', () => {
   let sut: ReserveTool
   const toolsRepository = mock<LoadToolByIdRepository >()
-  const { id, mechanicName } = toolsParams
-  const dateOfCollection = new Date('2023-10-13')
-  const dateOfDevolution = new Date('2023-10-28')
+  const { id, mechanicName, name, description, status } = toolsParams
+  const dateOfCollection = '2023-10-13'
+  const dateOfDevolution = '2023-10-28'
   const StringId = String(id)
+
+  beforeAll(() => {
+    toolsRepository.loadById.mockResolvedValue({ id, name, dateOfCollection: new Date(dateOfCollection), dateOfDevolution: new Date(dateOfDevolution), description, mechanicName, status })
+  })
 
   beforeEach(() => {
     sut = new ReserveToolUseCase(toolsRepository)
@@ -20,5 +25,13 @@ describe('ReserveToolUseCase', () => {
 
     expect(toolsRepository.loadById).toHaveBeenCalledWith({ id })
     expect(toolsRepository.loadById).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw NotFoundError if LoadToolByIdRepository return undefined', async () => {
+    toolsRepository.loadById.mockResolvedValueOnce(undefined)
+
+    const promise = sut.reserveTool({ id: StringId, dateOfCollection, dateOfDevolution, mechanicName })
+
+    await expect(promise).rejects.toThrow(new NotFoundError('tool'))
   })
 })
