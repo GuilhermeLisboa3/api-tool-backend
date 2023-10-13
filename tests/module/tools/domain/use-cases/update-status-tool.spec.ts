282e4439d6d330250caa2422bf2eefd9bacd9f08
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-13 16:10:18 -0300

    test: ensure UpdateStatusToolUseCase call UpdateToolRepo with correct values if status has available

--- tests/module/tools/domain/use-cases/update-status-tool.spec.ts
+++ tests/module/tools/domain/use-cases/update-status-tool.spec.ts
@@ -1,13 +1,13 @@
 import { NotFoundError } from '@/common/errors'
 import { type UpdateStatusTool, UpdateStatusToolUseCase } from '@/module/tools/domain/use-cases'
 import { toolsParams } from '@/tests/mocks'
-import { type LoadToolByIdRepository } from '@/module/tools/domain/contracts/database/tools'
+import { type UpdateToolRepository, type LoadToolByIdRepository } from '@/module/tools/domain/contracts/database/tools'
 
 import { mock } from 'jest-mock-extended'
 
 describe('UpdateStatusToolUseCase', () => {
   let sut: UpdateStatusTool
-  const toolsRepository = mock<LoadToolByIdRepository>()
+  const toolsRepository = mock<LoadToolByIdRepository & UpdateToolRepository>()
   const { id, name, dateOfCollection, dateOfDevolution, description, mechanicName, status } = toolsParams
   const StringId = String(id)
 
@@ -20,7 +20,7 @@ describe('UpdateStatusToolUseCase', () => {
   })
 
   it('should call LoadToolByIdRepository with correct values', async () => {
-    await sut.update({ id: StringId })
+    await sut.update({ id: StringId, status: 'inUse' })
 
     expect(toolsRepository.loadById).toHaveBeenCalledWith({ id })
     expect(toolsRepository.loadById).toHaveBeenCalledTimes(1)
@@ -29,8 +29,15 @@ describe('UpdateStatusToolUseCase', () => {
   it('should throw NotFoundError if LoadToolByIdRepository return undefined', async () => {
     toolsRepository.loadById.mockResolvedValueOnce(undefined)
 
-    const promise = sut.update({ id: StringId })
+    const promise = sut.update({ id: StringId, status: 'inUse' })
 
     await expect(promise).rejects.toThrow(new NotFoundError('tool'))
+  })
+
+  it('should call UpdateToolRepository with correct values if status has available', async () => {
+    await sut.update({ id: StringId, status: 'available' })
+
+    expect(toolsRepository.update).toHaveBeenCalledWith({ id, status: 'available', dateOfCollection: null, dateOfDevolution: null, mechanicName: null })
+    expect(toolsRepository.update).toHaveBeenCalledTimes(1)
   })
 })
