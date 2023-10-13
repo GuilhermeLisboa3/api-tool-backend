c5d53fdd6b8348ed69366403be73357574d163d0
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-13 01:28:37 -0300

    feat: ensure route GET /tools return 200 on success

--- src/module/tools/controllers/tools.controller.ts
+++ src/module/tools/controllers/tools.controller.ts
@@ -1,18 +1,25 @@
 import { AddToolDto } from './dto'
-import { AddTool } from '../domain/use-cases'
+import { AddTool, ListTools } from '../domain/use-cases'
 
-import { Body, Controller, Post, Res } from '@nestjs/common'
+import { Body, Controller, Get, Post, Res } from '@nestjs/common'
 import { type Response } from 'express'
 
 @Controller('')
 export class ToolsController {
   constructor (
-    private readonly addTool: AddTool
+    private readonly addTool: AddTool,
+    private readonly listTools: ListTools
   ) {}
 
   @Post('register')
   async create (@Body() input: AddToolDto, @Res() res: Response): Promise<Response> {
     await this.addTool.add(input)
     return res.status(201).json(null)
+  }
+
+  @Get('tools')
+  async list (@Res() res: Response): Promise<Response> {
+    const result = await this.listTools.list()
+    return res.status(200).json(result)
   }
 }
--- src/module/tools/tools.module.ts
+++ src/module/tools/tools.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ToolsController } from '@/module/tools/controllers/tools.controller'
 import { ToolsRepository } from '@/module/tools/infra/database/postgres/repositories'
-import { AddTool, AddToolUseCase } from '@/module/tools/domain/use-cases/add-tool.service'
+import { AddTool, AddToolUseCase, ListTools, ListToolsUseCase } from '@/module/tools/domain/use-cases/'
 
 @Module({
   controllers: [ToolsController],
@@ -9,6 +9,10 @@ import { AddTool, AddToolUseCase } from '@/module/tools/domain/use-cases/add-too
     {
       provide: AddTool,
       useClass: AddToolUseCase
+    },
+    {
+      provide: ListTools,
+      useClass: ListToolsUseCase
     },
     {
       provide: 'repository',
