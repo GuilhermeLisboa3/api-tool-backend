e72348902960a5192161f77757d936647943a6ac
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-13 12:07:32 -0300

    test: ensure DeleteToolByIdUseCase call DeleteToolByIdRepository with correct values

--- tests/module/tools/domain/use-cases/delete-tool.spec.ts
+++ tests/module/tools/domain/use-cases/delete-tool.spec.ts
@@ -1,12 +1,12 @@
 import { type DeleteToolById, DeleteToolByIdUseCase } from '@/module/tools/domain/use-cases'
 import { toolsParams } from '@/tests/mocks'
 import { mock } from 'jest-mock-extended'
-import { type LoadToolByIdRepository } from '@/module/tools/domain/contracts/database/tools'
+import { type LoadToolByIdRepository, type DeleteToolByIdRepository } from '@/module/tools/domain/contracts/database/tools'
 import { NotFoundError } from '@/common/errors'
 
 describe('DeleteToolUseCase', () => {
   let sut: DeleteToolById
-  const toolsRepository = mock<LoadToolByIdRepository>()
+  const toolsRepository = mock<LoadToolByIdRepository & DeleteToolByIdRepository>()
   const { id, name, dateOfCollection, dateOfDevolution, description, mechanicName, status } = toolsParams
   const StringId = String(id)
 
@@ -31,5 +31,12 @@ describe('DeleteToolUseCase', () => {
     const promise = sut.delete({ id: StringId })
 
     await expect(promise).rejects.toThrow(new NotFoundError('tool'))
+  })
+
+  it('should call DeleteToolByIdRepository with correct values', async () => {
+    await sut.delete({ id: StringId })
+
+    expect(toolsRepository.deleteById).toHaveBeenCalledWith({ id })
+    expect(toolsRepository.deleteById).toHaveBeenCalledTimes(1)
   })
 })
