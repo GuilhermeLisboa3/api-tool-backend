f0849eb272ed21449880e0e8326fed1e810f2026
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-13 00:38:36 -0300

    feat: ensure AddToolUseCase call CreateToolRepository with correct values

--- src/module/tools/domain/use-cases/add-tool.service.ts
+++ src/module/tools/domain/use-cases/add-tool.service.ts
@@ -1,6 +1,16 @@
+import { type CreateToolRepository } from '../contracts/database/tools'
+
 type Input = { name: string, description: string }
 type Output = void
 
 export abstract class AddTool {
   abstract add (input: Input): Promise<Output>
+}
+
+export class AddToolUseCase implements AddTool {
+  constructor (private readonly toolsRepository: CreateToolRepository) {}
+
+  async add (input: Input): Promise<void> {
+    await this.toolsRepository.create(input)
+  }
 }
