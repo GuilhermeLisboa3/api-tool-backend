6a6354bbe7d8c4644e878d60fee8822a891918cf
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-10-13 01:43:50 -0300

    feat: ensure ToolsRepository return tool on success

--- src/module/tools/infra/database/postgres/repositories/tools.service.ts
+++ src/module/tools/infra/database/postgres/repositories/tools.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common'
-import { type CreateToolRepository, type ListToolsRepository } from '@/module/tools/domain/contracts/database/tools'
+import { type CreateToolRepository, type ListToolsRepository, type LoadToolByIdRepository } from '@/module/tools/domain/contracts/database/tools'
 import prisma from '@/config/prisma'
 
 @Injectable()
-export class ToolsRepository implements CreateToolRepository, ListToolsRepository {
+export class ToolsRepository implements CreateToolRepository, ListToolsRepository, LoadToolByIdRepository {
   async create ({ description, name }: CreateToolRepository.Input): Promise<CreateToolRepository.Output> {
     await prisma.tool.create({ data: { description, name, status: 'available' } })
   }
@@ -11,5 +11,10 @@ export class ToolsRepository implements CreateToolRepository, ListToolsRepositor
   async list (): Promise<ListToolsRepository.Output> {
     const result = await prisma.tool.findMany()
     return result
+  }
+
+  async loadById ({ id }: LoadToolByIdRepository.Input): Promise<LoadToolByIdRepository.Output> {
+    const tool = await prisma.tool.findFirst({ where: { id } })
+    return tool ?? undefined
   }
 }
